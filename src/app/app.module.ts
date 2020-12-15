a5351440996ed293e4787483ea6f344d2c9672c9
Oswaldo Garcia Sosa <oswaldo.garcia_10@outlook.com>
2020-12-14 19:39:21 -0600

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,26 @@ import { AppComponent } from './app.component';
 import { GuiComponent } from './gui/gui.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { StorageService } from "../services/storage.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    GuiComponent
+    GuiComponent,
+    LoginComponent
   ],
   imports: [
+    HttpClientModule, 
     BrowserModule,
     BrowserAnimationsModule,
     MatSelectModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
